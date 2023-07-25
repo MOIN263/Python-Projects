51b2ecab932e688685e7ea88d7aee17ba6197004
MOIN263 <smoinullah1@gmail.com>
2023-07-25 22:42:33 +0530

    Practice

---
+++ PassthePigsGame/1.py
@@ -0,0 +1,12 @@
+# to take the input of number of players 
+
+players = input('total number of players (2-4) :')
+print(players)
+
+# check whether the given input is a digit or not 
+
+if players.isdigit() :
+    print(True)
+    
+else :
+    print(False)
